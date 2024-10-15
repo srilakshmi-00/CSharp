e88529c1670b6f6d7ae181554db0f37eea7627fc
Sri Lakshmi Tirupatamma <122328661+srilakshmi-00@users.noreply.github.com>
2024-10-15 15:59:54 +0530

    Add files via upload

---
+++ pro1.cs
@@ -0,0 +1,19 @@
+using System;
+class Test{
+static void Main(string[] args){
+int a=29;
+long b=a;
+Console.WriteLine(b.GetType());
+Console.WriteLine(b);
+long l=29374983983;
+float c=a;
+Console.WriteLine(c);
+Console.WriteLine(c.GetType());
+float f=29.3f;
+double d=a;
+Console.WriteLine(d);
+Console.WriteLine(d.GetType());
+Console.WriteLine("Enter an Interger");
+
+}
+}
